f8abc818f34560af455fabc58e04644b51cbc253
shravan <shravandas191@gmail.com>
2026-03-01 13:38:26 +0530

    color

--- online menu/src/Pages/admin/AdminLogin.jsx
+++ online menu/src/Pages/admin/AdminLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./AdminPage.css";
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
